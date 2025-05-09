e2817cd2d671f659ac8052c6501efbf53f7c39f0
Capucine Dausse <capucine.dausse@gmail.com>
2025-05-09 10:20:56 +0200

    Continu volet 2 (début partie avec tableaux à remplir)

--- ExtinctHeure/ExtinctHeure/frmAccueil.Designer.cs
+++ ExtinctHeure/ExtinctHeure/frmAccueil.Designer.cs
@@ -29,28 +29,28 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.lblNumMis = new System.Windows.Forms.Label();
             this.lblDteMis = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.txtMotif = new System.Windows.Forms.TextBox();
-            this.txtRue = new System.Windows.Forms.TextBox();
-            this.txtCP = new System.Windows.Forms.TextBox();
-            this.txtVille = new System.Windows.Forms.TextBox();
-            this.btnAnn = new System.Windows.Forms.Button();
-            this.btnConstEqu = new System.Windows.Forms.Button();
-            this.cboNatSin = new System.Windows.Forms.ComboBox();
+            this.lblNumMis = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.cboCasMob = new System.Windows.Forms.ComboBox();
+            this.cboNatSin = new System.Windows.Forms.ComboBox();
+            this.btnConstEqu = new System.Windows.Forms.Button();
+            this.btnAnn = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtVille = new System.Windows.Forms.TextBox();
+            this.txtCP = new System.Windows.Forms.TextBox();
+            this.txtRue = new System.Windows.Forms.TextBox();
+            this.txtMotif = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -65,6 +65,92 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
+            // 
+            // lblDteMis
+            // 
+            this.lblDteMis.AutoSize = true;
+            this.lblDteMis.Location = new System.Drawing.Point(712, 28);
+            this.lblDteMis.Name = "lblDteMis";
+            this.lblDteMis.Size = new System.Drawing.Size(110, 22);
+            this.lblDteMis.TabIndex = 4;
+            this.lblDteMis.Text = "déclenchée le : ";
+            // 
+            // lblNumMis
+            // 
+            this.lblNumMis.AutoSize = true;
+            this.lblNumMis.Location = new System.Drawing.Point(269, 28);
+            this.lblNumMis.Name = "lblNumMis";
+            this.lblNumMis.Size = new System.Drawing.Size(78, 22);
+            this.lblNumMis.TabIndex = 3;
+            this.lblNumMis.Text = "Mission n°";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cboCasMob);
+            this.groupBox3.Controls.Add(this.cboNatSin);
+            this.groupBox3.Controls.Add(this.btnConstEqu);
+            this.groupBox3.Controls.Add(this.btnAnn);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Location = new System.Drawing.Point(30, 245);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1151, 176);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Décisions du régulateur";
+            // 
+            // cboCasMob
+            // 
+            this.cboCasMob.FormattingEnabled = true;
+            this.cboCasMob.Location = new System.Drawing.Point(827, 49);
+            this.cboCasMob.Name = "cboCasMob";
+            this.cboCasMob.Size = new System.Drawing.Size(193, 30);
+            this.cboCasMob.TabIndex = 11;
+            // 
+            // cboNatSin
+            // 
+            this.cboNatSin.FormattingEnabled = true;
+            this.cboNatSin.Location = new System.Drawing.Point(296, 49);
+            this.cboNatSin.Name = "cboNatSin";
+            this.cboNatSin.Size = new System.Drawing.Size(193, 30);
+            this.cboNatSin.TabIndex = 10;
+            // 
+            // btnConstEqu
+            // 
+            this.btnConstEqu.Location = new System.Drawing.Point(1023, 109);
+            this.btnConstEqu.Name = "btnConstEqu";
+            this.btnConstEqu.Size = new System.Drawing.Size(122, 61);
+            this.btnConstEqu.TabIndex = 9;
+            this.btnConstEqu.Text = "Constituer équipe";
+            this.btnConstEqu.UseVisualStyleBackColor = true;
+            this.btnConstEqu.Click += new System.EventHandler(this.btnConstEqu_Click);
+            // 
+            // btnAnn
+            // 
+            this.btnAnn.Location = new System.Drawing.Point(895, 109);
+            this.btnAnn.Name = "btnAnn";
+            this.btnAnn.Size = new System.Drawing.Size(122, 61);
+            this.btnAnn.TabIndex = 8;
+            this.btnAnn.Text = "Annuler";
+            this.btnAnn.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(682, 48);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(139, 22);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Caserne à mobiliser";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(174, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 22);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Nature du siniste";
             // 
             // groupBox2
             // 
@@ -84,56 +170,37 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Informations usager";
             // 
-            // groupBox3
+            // txtVille
             // 
-            this.groupBox3.Controls.Add(this.cboCasMob);
-            this.groupBox3.Controls.Add(this.cboNatSin);
-            this.groupBox3.Controls.Add(this.btnConstEqu);
-            this.groupBox3.Controls.Add(this.btnAnn);
-            this.groupBox3.Controls.Add(this.label9);
-            this.groupBox3.Controls.Add(this.label8);
-            this.groupBox3.Location = new System.Drawing.Point(30, 245);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(1151, 176);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Décisions du régulateur";
+            this.txtVille.Location = new System.Drawing.Point(751, 122);
+            this.txtVille.Name = "txtVille";
+            this.txtVille.Size = new System.Drawing.Size(198, 28);
+            this.txtVille.TabIndex = 8;
+            this.txtVille.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtVille_KeyPress);
             // 
-            // label1
+            // txtCP
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(215, 57);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Motif";
+            this.txtCP.Location = new System.Drawing.Point(751, 88);
+            this.txtCP.Name = "txtCP";
+            this.txtCP.Size = new System.Drawing.Size(198, 28);
+            this.txtCP.TabIndex = 7;
+            this.txtCP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCP_KeyPress);
             // 
-            // label2
+            // txtRue
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(685, 57);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Rue";
+            this.txtRue.Location = new System.Drawing.Point(751, 54);
+            this.txtRue.Name = "txtRue";
+            this.txtRue.Size = new System.Drawing.Size(198, 28);
+            this.txtRue.TabIndex = 6;
+            this.txtRue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRue_KeyPress);
             // 
-            // label3
+            // txtMotif
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(635, 91);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 22);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Code postal";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(685, 125);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 22);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Ville";
+            this.txtMotif.Location = new System.Drawing.Point(274, 54);
+            this.txtMotif.Name = "txtMotif";
+            this.txtMotif.Size = new System.Drawing.Size(198, 28);
+            this.txtMotif.TabIndex = 5;
+            this.txtMotif.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMotif_KeyPress);
             // 
             // label5
             // 
@@ -144,120 +211,60 @@
             this.label5.TabIndex = 4;
             this.label5.Text = "Adresse sinistre";
             // 
-            // lblNumMis
+            // label4
             // 
-            this.lblNumMis.AutoSize = true;
-            this.lblNumMis.Location = new System.Drawing.Point(269, 28);
-            this.lblNumMis.Name = "lblNumMis";
-            this.lblNumMis.Size = new System.Drawing.Size(78, 22);
-            this.lblNumMis.TabIndex = 3;
-            this.lblNumMis.Text = "Mission n°";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(685, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ville";
             // 
-            // lblDteMis
+            // label3
             // 
-            this.lblDteMis.AutoSize = true;
-            this.lblDteMis.Location = new System.Drawing.Point(712, 28);
-            this.lblDteMis.Name = "lblDteMis";
-            this.lblDteMis.Size = new System.Drawing.Size(110, 22);
-            this.lblDteMis.TabIndex = 4;
-            this.lblDteMis.Text = "déclenchée le : ";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(635, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Code postal";
             // 
-            // label8
+            // label2
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(174, 48);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(116, 22);
-            this.label8.TabIndex = 4;
-            this.label8.Text = "Nature du siniste";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rue";
             // 
-            // label9
+            // label1
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(682, 48);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(139, 22);
-            this.label9.TabIndex = 5;
-            this.label9.Text = "Caserne à mobiliser";
-            // 
-            // txtMotif
-            // 
-            this.txtMotif.Location = new System.Drawing.Point(274, 54);
-            this.txtMotif.Name = "txtMotif";
-            this.txtMotif.Size = new System.Drawing.Size(198, 28);
-            this.txtMotif.TabIndex = 5;
-            // 
-            // txtRue
-            // 
-            this.txtRue.Location = new System.Drawing.Point(751, 54);
-            this.txtRue.Name = "txtRue";
-            this.txtRue.Size = new System.Drawing.Size(198, 28);
-            this.txtRue.TabIndex = 6;
-            // 
-            // txtCP
-            // 
-            this.txtCP.Location = new System.Drawing.Point(751, 88);
-            this.txtCP.Name = "txtCP";
-            this.txtCP.Size = new System.Drawing.Size(198, 28);
-            this.txtCP.TabIndex = 7;
-            // 
-            // txtVille
-            // 
-            this.txtVille.Location = new System.Drawing.Point(751, 122);
-            this.txtVille.Name = "txtVille";
-            this.txtVille.Size = new System.Drawing.Size(198, 28);
-            this.txtVille.TabIndex = 8;
-            // 
-            // btnAnn
-            // 
-            this.btnAnn.Location = new System.Drawing.Point(895, 109);
-            this.btnAnn.Name = "btnAnn";
-            this.btnAnn.Size = new System.Drawing.Size(122, 61);
-            this.btnAnn.TabIndex = 8;
-            this.btnAnn.Text = "Annuler";
-            this.btnAnn.UseVisualStyleBackColor = true;
-            // 
-            // btnConstEqu
-            // 
-            this.btnConstEqu.Location = new System.Drawing.Point(1023, 109);
-            this.btnConstEqu.Name = "btnConstEqu";
-            this.btnConstEqu.Size = new System.Drawing.Size(122, 61);
-            this.btnConstEqu.TabIndex = 9;
-            this.btnConstEqu.Text = "Constituer équipe";
-            this.btnConstEqu.UseVisualStyleBackColor = true;
-            // 
-            // cboNatSin
-            // 
-            this.cboNatSin.FormattingEnabled = true;
-            this.cboNatSin.Location = new System.Drawing.Point(296, 49);
-            this.cboNatSin.Name = "cboNatSin";
-            this.cboNatSin.Size = new System.Drawing.Size(193, 30);
-            this.cboNatSin.TabIndex = 10;
-            // 
-            // cboCasMob
-            // 
-            this.cboCasMob.FormattingEnabled = true;
-            this.cboCasMob.Location = new System.Drawing.Point(850, 48);
-            this.cboCasMob.Name = "cboCasMob";
-            this.cboCasMob.Size = new System.Drawing.Size(193, 30);
-            this.cboCasMob.TabIndex = 11;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Motif";
             // 
             // frmAccueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1271, 696);
+            this.ClientSize = new System.Drawing.Size(1269, 704);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmAccueil";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmAccueil_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
--- ExtinctHeure/ExtinctHeure/frmAccueil.cs
+++ ExtinctHeure/ExtinctHeure/frmAccueil.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Pinpon;
 
 namespace ExtinctHeure
 {
@@ -16,5 +19,128 @@ namespace ExtinctHeure
         {
             InitializeComponent();
         }
+
+        private void frmAccueil_Load(object sender, EventArgs e)
+        {
+            SQLiteConnection cx = Connexion.Connec;
+            DataTable dt = cx.GetSchema("Tables");
+
+            foreach (DataRow ligne in dt.Rows)
+            {
+                string requete = "select * from " + ligne[2];
+                SQLiteCommand cd = new SQLiteCommand(requete, cx);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(cd);
+
+                da.Fill(MesDatas.DsGlobal, ligne[2].ToString());
+            }
+
+            Connexion.FermerConnexion();
+
+
+            lblDteMis.Text += " " + DateTime.Now.ToString();
+
+
+            int i = 0;
+            foreach (DataRow dr in MesDatas.DsGlobal.Tables["Mission"].Rows)
+            {
+                if (i < Convert.ToInt32(dr["id"]))
+                {
+                    i = Convert.ToInt32(dr["id"]);
+                }
+            }
+            i++;
+            lblNumMis.Text += " " + i.ToString();
+
+
+            foreach (DataRow dr in MesDatas.DsGlobal.Tables["Caserne"].Rows)
+            {
+                int cas = Convert.ToInt32(dr["id"]);
+                cboCasMob.Items.Add(cas);
+            }
+
+            int j = 1;
+            foreach (DataRow dr in MesDatas.DsGlobal.Tables["NatureSinistre"].Rows)
+            {
+                string lib = dr["libelle"].ToString();
+                cboNatSin.Tag = j;
+                j++;
+                cboNatSin.Items.Add(lib);
+            }
+        }
+
+        private void txtRue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if ((char.IsLetter(e.KeyChar)) || (char.IsNumber(e.KeyChar)))
+            {
+                e.Handled = false;
+            }
+            if ((e.KeyChar == (char)Keys.Tab) || (e.KeyChar == (char)Keys.Enter))
+            {
+                txtCP.Focus();
+            }
+        }
+
+        private void txtCP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if ((e.KeyChar == (char)Keys.Back) || (e.KeyChar == (char)Keys.Enter) || (e.KeyChar == (char)Keys.Tab))
+            {
+                e.Handled = false;
+            }
+            if (txtCP.Text.Length < 2)
+            {
+                if (char.IsNumber(e.KeyChar))
+                {
+                    e.Handled = false;
+                }
+            }
+            if ((e.KeyChar == (char)Keys.Tab) || (e.KeyChar == (char)Keys.Enter))
+            {
+                txtVille.Focus();
+            }
+        }
+
+        private void txtVille_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if (char.IsLetter(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+        }
+
+        private void txtMotif_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if ((e.KeyChar == (char)Keys.Tab) || (e.KeyChar == (char)Keys.Enter))
+            {
+                txtRue.Focus();
+            }
+        }
+
+        private void btnConstEqu_Click(object sender, EventArgs e)
+        {
+            ArrayList Vehicule = new ArrayList();
+            ArrayList NbrVehi = new ArrayList();
+
+            int idSin = cboNatSin.SelectedIndex + 1;
+            foreach (DataRow dr in MesDatas.DsGlobal.Tables["Necessiter"].Rows)
+            {
+                if (Convert.ToInt32(dr["idNatureSinistre"]) == idSin)
+                {
+
+                    Vehicule.Add(dr["codeTypeEngin"]);
+                }
+            }
+        }
+
+        //Pour trouver le bon véhicule à emmener :
+        //cboTag id (nature sinistre) -> codetype et nombre (necessiter)
+        //-> id caserne, numero, enmission et enpanne (engin)
+
+
+
+
     }
 }
