02d9638648cd725fb9c4f26b331300c4bdfc6bcf
Bebel9445 <bebel9445@gmail.com>
2025-05-12 17:31:17 +0200

    Ajout des stats individuelles

--- ExtinctHeure/ExtinctHeure/frmAccueil.Designer.cs
+++ ExtinctHeure/ExtinctHeure/frmAccueil.Designer.cs
@@ -28,22 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpStat = new System.Windows.Forms.GroupBox();
+            this.cboCaserneStat = new System.Windows.Forms.ComboBox();
+            this.dgvEnginPlusUtilise = new System.Windows.Forms.DataGridView();
+            this.lblEnginNbUtilisation = new System.Windows.Forms.Label();
+            this.dgvEnginPlusUtiliseHeure = new System.Windows.Forms.DataGridView();
+            this.lblEnginNbHeure = new System.Windows.Forms.Label();
+            this.grpStat.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnginPlusUtilise)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnginPlusUtiliseHeure)).BeginInit();
             this.SuspendLayout();
+            // 
+            // grpStat
+            // 
+            this.grpStat.Controls.Add(this.lblEnginNbHeure);
+            this.grpStat.Controls.Add(this.lblEnginNbUtilisation);
+            this.grpStat.Controls.Add(this.dgvEnginPlusUtiliseHeure);
+            this.grpStat.Controls.Add(this.dgvEnginPlusUtilise);
+            this.grpStat.Controls.Add(this.cboCaserneStat);
+            this.grpStat.Location = new System.Drawing.Point(12, 103);
+            this.grpStat.Name = "grpStat";
+            this.grpStat.Size = new System.Drawing.Size(1210, 746);
+            this.grpStat.TabIndex = 0;
+            this.grpStat.TabStop = false;
+            this.grpStat.Text = "Statistiques";
+            this.grpStat.VisibleChanged += new System.EventHandler(this.grpStat_VisibleChanged);
+            // 
+            // cboCaserneStat
+            // 
+            this.cboCaserneStat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCaserneStat.FormattingEnabled = true;
+            this.cboCaserneStat.Location = new System.Drawing.Point(456, 24);
+            this.cboCaserneStat.Name = "cboCaserneStat";
+            this.cboCaserneStat.Size = new System.Drawing.Size(239, 25);
+            this.cboCaserneStat.TabIndex = 0;
+            this.cboCaserneStat.SelectedIndexChanged += new System.EventHandler(this.cboCaserneStat_SelectedIndexChanged);
+            // 
+            // dgvEnginPlusUtilise
+            // 
+            this.dgvEnginPlusUtilise.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEnginPlusUtilise.Location = new System.Drawing.Point(22, 150);
+            this.dgvEnginPlusUtilise.Name = "dgvEnginPlusUtilise";
+            this.dgvEnginPlusUtilise.Size = new System.Drawing.Size(398, 219);
+            this.dgvEnginPlusUtilise.TabIndex = 1;
+            // 
+            // lblEnginNbUtilisation
+            // 
+            this.lblEnginNbUtilisation.AutoSize = true;
+            this.lblEnginNbUtilisation.Location = new System.Drawing.Point(112, 130);
+            this.lblEnginNbUtilisation.Name = "lblEnginNbUtilisation";
+            this.lblEnginNbUtilisation.Size = new System.Drawing.Size(210, 17);
+            this.lblEnginNbUtilisation.TabIndex = 2;
+            this.lblEnginNbUtilisation.Text = "Nombre d\'utilisation des engins";
+            // 
+            // dgvEnginPlusUtiliseHeure
+            // 
+            this.dgvEnginPlusUtiliseHeure.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEnginPlusUtiliseHeure.Location = new System.Drawing.Point(789, 150);
+            this.dgvEnginPlusUtiliseHeure.Name = "dgvEnginPlusUtiliseHeure";
+            this.dgvEnginPlusUtiliseHeure.Size = new System.Drawing.Size(398, 219);
+            this.dgvEnginPlusUtiliseHeure.TabIndex = 1;
+            // 
+            // lblEnginNbHeure
+            // 
+            this.lblEnginNbHeure.AutoSize = true;
+            this.lblEnginNbHeure.Location = new System.Drawing.Point(857, 130);
+            this.lblEnginNbHeure.Name = "lblEnginNbHeure";
+            this.lblEnginNbHeure.Size = new System.Drawing.Size(262, 17);
+            this.lblEnginNbHeure.TabIndex = 2;
+            this.lblEnginNbHeure.Text = "Nombre d\'heure d\'utilisation des engins";
             // 
             // frmAccueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1234, 861);
+            this.Controls.Add(this.grpStat);
             this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAccueil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Extinct\'Heure";
+            this.Load += new System.EventHandler(this.frmAccueil_Load);
+            this.grpStat.ResumeLayout(false);
+            this.grpStat.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnginPlusUtilise)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnginPlusUtiliseHeure)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpStat;
+        private System.Windows.Forms.ComboBox cboCaserneStat;
+        private System.Windows.Forms.Label lblEnginNbUtilisation;
+        private System.Windows.Forms.DataGridView dgvEnginPlusUtilise;
+        private System.Windows.Forms.Label lblEnginNbHeure;
+        private System.Windows.Forms.DataGridView dgvEnginPlusUtiliseHeure;
     }
 }
 
--- ExtinctHeure/ExtinctHeure/frmAccueil.cs
+++ ExtinctHeure/ExtinctHeure/frmAccueil.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Pinpon;
 
 namespace ExtinctHeure
 {
@@ -15,6 +17,70 @@ namespace ExtinctHeure
         public frmAccueil()
         {
             InitializeComponent();
+        }
+
+        SQLiteConnection cx = Connexion.Connec;
+
+        private void frmAccueil_Load(object sender, EventArgs e)
+        {
+            String requete = "SELECT nom FROM Caserne";
+            SQLiteCommand cmd = new SQLiteCommand(requete, cx);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                String nom = reader.GetString(0);
+                cboCaserneStat.Items.Add(nom);
+            }
+            dgvEnginPlusUtilise.Columns.Add("Nom", "Nom de l'engin");
+            dgvEnginPlusUtilise.Columns.Add("NbUtilisation", "Nombre d'utilisation");
+            dgvEnginPlusUtiliseHeure.Columns.Add("Nom", "Nom de l'engin");
+            dgvEnginPlusUtiliseHeure.Columns.Add("NbHeure", "Nombre d'heures");
+
+            dgvEnginPlusUtilise.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvEnginPlusUtiliseHeure.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            cboCaserneStat.SelectedIndex = 0;
+        }
+
+        private void grpStat_VisibleChanged(object sender, EventArgs e)
+        {
+            if (grpStat.Visible)
+            {
+
+            }
+        }
+
+        private void cboCaserneStat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String requete = $"select codeTypeEngin || numeroEngin, count(*) from PartirAvec where idCaserne = {cboCaserneStat.SelectedIndex + 1} group by codeTypeEngin, numeroEngin order by count(*) desc";
+            SQLiteCommand cmd = new SQLiteCommand(requete, cx);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            dgvEnginPlusUtilise.Rows.Clear();
+            while (reader.Read())
+            {
+                String nom = reader.GetString(0);
+                int nbUtilisation = reader.GetInt32(1);
+                dgvEnginPlusUtilise.Rows.Add(nom, nbUtilisation);
+            }
+
+            requete = $"select pa.codeTypeEngin || pa.numeroEngin \"Engin\", sum(round((julianday(m.dateHeureRetour) - julianday(m.dateHeureDepart))*24)) \"temps passé\" from Mission m join PartirAvec pa on m.id = pa.idMission where pa.idCaserne = {cboCaserneStat.SelectedIndex + 1} group by Engin order by \"temps passé\" desc";
+            cmd = new SQLiteCommand(requete, cx);
+            reader = cmd.ExecuteReader();
+            dgvEnginPlusUtiliseHeure.Rows.Clear();
+            while (reader.Read())
+            {
+                int nbHeure;
+                if (reader.IsDBNull(1))
+                {
+                    nbHeure = 0;
+                }
+                else
+                {
+                    nbHeure = Convert.ToInt32(reader.GetFloat(1));
+                }
+                String nom = reader.GetString(0);
+                dgvEnginPlusUtiliseHeure.Rows.Add(nom, nbHeure);
+            }
         }
     }
 }
