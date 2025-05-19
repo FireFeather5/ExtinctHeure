763ff8b00b5ddc79a1875d95aa955b999701b80b
Bebel9445 <bebel9445@gmail.com>
2025-05-19 12:27:59 +0200

    Debut du volet 1 et creation du User Control

--- ExtinctHeure/ExtinctHeure/frmAccueil.Designer.cs
+++ ExtinctHeure/ExtinctHeure/frmAccueil.Designer.cs
@@ -28,22 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpMissions = new System.Windows.Forms.GroupBox();
+            this.mission1 = new ExtinctHeureUC.Mission();
+            this.grpMissions.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grpMissions
+            // 
+            this.grpMissions.Controls.Add(this.mission1);
+            this.grpMissions.Location = new System.Drawing.Point(12, 155);
+            this.grpMissions.Name = "grpMissions";
+            this.grpMissions.Size = new System.Drawing.Size(1210, 694);
+            this.grpMissions.TabIndex = 0;
+            this.grpMissions.TabStop = false;
+            this.grpMissions.Text = "Missions";
+            this.grpMissions.VisibleChanged += new System.EventHandler(this.grpMissions_VisibleChanged);
+            // 
+            // mission1
+            // 
+            this.mission1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mission1.caserne = "*";
+            this.mission1.debutMission = "*";
+            this.mission1.estFini = false;
+            this.mission1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mission1.idMission = -1;
+            this.mission1.imageMission = null;
+            this.mission1.Location = new System.Drawing.Point(7, 30);
+            this.mission1.Margin = new System.Windows.Forms.Padding(4);
+            this.mission1.Name = "mission1";
+            this.mission1.natureMission = "*";
+            this.mission1.nomMission = "*";
+            this.mission1.Size = new System.Drawing.Size(1198, 150);
+            this.mission1.TabIndex = 0;
             // 
             // frmAccueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1234, 861);
+            this.Controls.Add(this.grpMissions);
             this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAccueil";
             this.Text = "Extinct\'Heure";
+            this.Load += new System.EventHandler(this.frmAccueil_Load);
+            this.grpMissions.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpMissions;
+        private ExtinctHeureUC.Mission mission1;
     }
 }
 
--- ExtinctHeure/ExtinctHeure/frmAccueil.cs
+++ ExtinctHeure/ExtinctHeure/frmAccueil.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExtinctHeureUC;
+using Pinpon;
 
 namespace ExtinctHeure
 {
@@ -15,6 +18,48 @@ namespace ExtinctHeure
         public frmAccueil()
         {
             InitializeComponent();
+        }
+
+        SQLiteConnection cx = Connexion.Connec;
+        DataSet monDS = MesDatas.DsGlobal;
+
+        private void frmAccueil_Load(object sender, EventArgs e)
+        {
+            DataTable SchemaTable = cx.GetSchema("Tables");
+            foreach (DataRow row in SchemaTable.Rows)
+            {
+                String requete = "SELECT * FROM " + row["TABLE_NAME"].ToString();
+                SQLiteCommand cmd = new SQLiteCommand(requete, cx);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+
+                da.Fill(monDS, row["TABLE_NAME"].ToString());
+            }
+            Connexion.FermerConnexion();
+
+            ForeignKeyConstraint FK_Mission_idCaserne = new ForeignKeyConstraint("FK_Mission_idCaserne", monDS.Tables["Caserne"].Columns["id"], monDS.Tables["Mission"].Columns["idCaserne"]);
+            ForeignKeyConstraint FK_Mission_idNature = new ForeignKeyConstraint("FK_Mission_idNature", monDS.Tables["NatureSinistre"].Columns["id"], monDS.Tables["Mission"].Columns["idNatureSinistre"]);
+
+            monDS.Tables["Mission"].Constraints.Add(FK_Mission_idCaserne);
+            monDS.Tables["Mission"].Constraints.Add(FK_Mission_idNature);
+        }
+
+        private void grpMissions_VisibleChanged(object sender, EventArgs e)
+        {
+            int position = 30; // + 160
+            foreach (DataRow dataRow in monDS.Tables["Mission"].Rows)
+            {
+                Mission mission = new Mission();
+                mission.idMission = Convert.ToInt32(dataRow["id"]);
+                mission.debutMission = dataRow["dateMission"].ToString();
+                mission.caserne = dataRow.GetParentRow("FK_Mission_idCaserne")["id"].ToString();
+                mission.natureMission = dataRow.GetParentRow("FK_Mission_idNature")["id"].ToString();
+                mission.nomMission = dataRow["motifAppel"].ToString();
+
+                mission.Location = new Point(7, position);
+                position += 160;
+
+                grpMissions.Controls.Add(mission);
+            }
         }
     }
 }
